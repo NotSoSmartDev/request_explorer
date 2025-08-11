c62ad7b3f20766d8e5c288bf216c0661e4fb6f96
Dzmitry Sianiuk <dsianyuk@octobrowser.net>
2025-08-11 23:20:41 +0300

    Fixed https link

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-uvicorn request_explorer.app:app --host 0.0.0.0 --port 8000 --proxy-headers
+uvicorn request_explorer.app:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips=*
